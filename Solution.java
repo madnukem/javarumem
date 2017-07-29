cc57d2320df5ce5edf295f65bab438f5dad978fb
Unknown <sbacqm@gmail.com>
2017-07-30 00:09:27 +0300

    123

---
+++ Solution.java
@@ -0,0 +1,47 @@
+package com.javarush.task.task13.task1319;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+
+/* 
+Писатель в файл с консоли
+1. Прочесть с консоли имя файла.
+2. Считывать строки с консоли, пока пользователь не введет строку «exit«.
+3. Вывести абсолютно все введенные строки в файл, каждую строчку с новой строки.
+
+
+Требования:
+1. Программа должна считывать c консоли имя файла.
+2. Создай и используй объект типа BufferedWriter.
+3. Программа должна читывать строки с консоли, пока пользователь не введет строку "exit".
+4. Программа должна записать абсолютно все введенные строки (включая "exit") в файл, каждую строчку с новой строки.
+5. Метод main должен закрывать объект типа BufferedWriter после использования.
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String fileName =/* "v:\\test.txt";//*/reader.readLine();
+        //System.out.println(fileName);
+        //OutputStream outputStream = new FileOutputStream(fileName);
+        BufferedWriter strrInput = new BufferedWriter(new FileWriter(fileName));
+        while (true){
+            String s = reader.readLine() + "\n";
+
+           InputStream strInput = new ByteArrayInputStream(s.getBytes(StandardCharsets.UTF_8));
+
+             /*       String carestka = "/n";
+            InputStream */
+            while (strInput.available() >0) {
+                int data = strInput.read();
+                strrInput.write(data);
+
+            }
+
+     strInput.close();
+            if (s.equals("exit\n")) break;
+        }
+        strrInput.close();
+
+    }
+}
